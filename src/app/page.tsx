aee07f79ec0bf057a125439414b9c0a7089b0312
Teddy <teddyhiny@gmail.com>
2025-06-09 20:53:53 +1000

    try relative path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 import HomeClient from '@/components/HomeClient';
 
 export default async function Home() {
-  const baseUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000';
-  const res = await fetch(`${baseUrl}/api/profile`, { cache: 'no-cache' });
-
+  const res = await fetch('/api/profile', { cache: 'no-cache' });
   const profileData = await res.json();
 
   return <HomeClient profileData={profileData} />;
