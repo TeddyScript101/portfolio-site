08889cc0686e50c3f86ecf112766d61ba687ddc3
Teddy <teddyhiny@gmail.com>
2025-06-09 21:13:35 +1000

    fix header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,30 @@
 import HomeClient from '@/components/HomeClient';
+import { cookies } from 'next/headers';
 
 export default async function Home() {
+  const baseUrl = process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
+    : 'http://localhost:3000';
+
+  const cookieStore = await cookies();
+  const cookieHeader = cookieStore.getAll().map(c => `${c.name}=${c.value}`).join('; ');
+
   try {
-    const baseUrl =
-      process.env.VERCEL_URL
-        ? `https://${process.env.VERCEL_URL}`
-        : 'http://localhost:3000';
+    const res = await fetch(`${baseUrl}/api/profile`, {
+      headers: {
+        cookie: cookieHeader
+      },
+      cache: 'no-cache'
+    });
 
-
-    console.log(`Base URL: ${baseUrl}`);
-    const res = await fetch(`${baseUrl}/api/profile`, { next: { revalidate: 0 } });
     if (!res.ok) {
       throw new Error(`Failed to fetch: ${res.status} ${res.statusText}`);
     }
 
     const profileData = await res.json();
     return <HomeClient profileData={profileData} />;
-
   } catch (error) {
     console.error('Error fetching profile data:', error);
-    return
+    return <div>Error loading profile.</div>;
   }
 }
