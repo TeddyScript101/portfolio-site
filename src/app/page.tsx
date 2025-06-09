3010e9927d3efdd5db8a083bf066b4f4128e5873
Teddy <teddyhiny@gmail.com>
2025-06-09 21:01:51 +1000

    debug baseur;

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,24 @@
 import HomeClient from '@/components/HomeClient';
 
 export default async function Home() {
-  const res = await fetch('/api/profile', { cache: 'no-cache' });
-  const profileData = await res.json();
+  try {
+    const baseUrl =
+      process.env.VERCEL_URL
+        ? `https://${process.env.VERCEL_URL}`
+        : 'http://localhost:3000';
 
-  return <HomeClient profileData={profileData} />;
+
+    console.log(`Base URL: ${baseUrl}`);
+    const res = await fetch(`${baseUrl}/api/profile`, { cache: 'no-cache' });
+    if (!res.ok) {
+      throw new Error(`Failed to fetch: ${res.status} ${res.statusText}`);
+    }
+
+    const profileData = await res.json();
+    return <HomeClient profileData={profileData} />;
+
+  } catch (error) {
+    console.error('Error fetching profile data:', error);
+    return
+  }
 }
